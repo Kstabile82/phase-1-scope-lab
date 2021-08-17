468d766ac078168200adaa02ba0b2fb41a725953
KarinaStabile <karina.stabile@gmail.com>
2021-08-16 21:18:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase(); 
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+function overwriteBestCustomer(input) {
+    bestCustomer = input; 
+}
+const leastFavoriteCustomer = 'Jane'; 
+function changeLeastFavoriteCustomer(input2) {
+    leastFavoriteCustomer = input2; 
+}
